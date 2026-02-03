2040a01b564ffb08647e33bcacba3749aa3d1952
GestionMuestrasUCV <gestionmuestra.ucv@gmail.com>
2026-02-03 16:17:34 -0400

    Revert "Shared Files"
    This reverts commit 8973c92a875d91a9095a064a36e71b9177a54939.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,28 +1,12 @@
-
-
-import { SharedRoutingModule } from './shared-routing.module';
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from '../app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
-import { RouterModule } from '@angular/router';
-import { ScanComponent } from './components/scan/scan.component';
 
 
 
 @NgModule({
-  declarations: [
-
-  ],
+  declarations: [],
   imports: [
     CommonModule
   ]
 })
-export class SharedModule  {
-  public Hello(){
-    console.log("hello")
-  }
-
-}
+export class SharedModule { }
